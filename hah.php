b1efeb27d270fcce658b7b3e95b3af1fbada29e7
raitkeer <raitkeer@greeny.cs.tlu.ee>
2016-09-14 14:49:38 +0300

    väike push

---
+++ hah.php
@@ -0,0 +1,35 @@
+<?php
+
+	$age = 5;
+	
+	if ($age < 18) {
+		echo "alaealine";
+	
+	}else {
+		echo "täisealine";
+		
+	}
+
+?>
+
+<br>
+
+<?php
+// for tsükkel: for (1;2;3) {sisu}
+// 1. kõige algus, $i = 0
+// 2. enne sisu iga kord, $i<$i
+// 3. pärast sisu, $i= $i+1
+
+	for ($i=0; $i < $age; $i = $i + 1) {
+		
+		echo ($i+1)."palju";
+		
+	}
+	
+	echo "õnne"
+
+
+
+
+
+?>
