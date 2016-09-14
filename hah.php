4bfc4948d9869821f8b8453762079395dea275b2
raitkeer <raitkeer@greeny.cs.tlu.ee>
2016-09-14 14:53:39 +0300

    väike täpsustus

--- hah.php
+++ hah.php
@@ -16,7 +16,7 @@
 
 <?php
 // for tsükkel: for (1;2;3) {sisu}
-// 1. kõige algus, $i = 0
+// 1. üks kord kõige algus, $i = 0
 // 2. enne sisu iga kord, $i<$i
 // 3. pärast sisu, $i= $i+1
 
@@ -27,9 +27,5 @@
 	}
 	
 	echo "õnne"
-
-
-
-
 
 ?>
